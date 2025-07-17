abad16c439b5814c1a6d6a45beffad1d59c10543
Yunus Burkut <yunus.burkut@outlook.com>
2025-07-17 12:56:05 +0300

    Current base scripts created

--- Assets/Scripts/Controllers/TetrominoController.cs
+++ Assets/Scripts/Controllers/TetrominoController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class TetrominoController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
