4a04279a47bf5c3f32829d03b4d7ee6567c0ef03
Yunus Burkut <yunus.burkut@outlook.com>
2025-07-17 12:59:54 +0300

    name change

--- Assets/Scripts/Managers/BoardManager.cs
+++ Assets/Scripts/Managers/BoardManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GridController : MonoBehaviour
+public class BoardManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
